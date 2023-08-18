3b8708476f022b634c1d728ed6c6dc4314f9cec2
hsnemlekar <nemlekar@usc.edu>
2023-08-18 16:41:43 -0700

    front page complete!

--- js/script.js
+++
@@ -1,6 +0,0 @@
-const mobileMenuToggle = document.getElementById("mobile-menu-toggle");
-const mobileDropdownMenu = document.getElementById("mobile-dropdown-menu");
-
-mobileMenuToggle.addEventListener("click", () => {
-  mobileDropdownMenu.classList.toggle("active");
-});
